e7614d22116b2551d0240ece82011df9cc5d1b88
Dan <peritract@hotmail.co.uk>
2023-11-09 15:10:40 +0000

    Fix everything Charlie did

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 """Functions to scrape book details from https://publishing.tor.com/."""
 
+from os import environ
 from time import sleep
 
+from dotenv import load_dotenv
 import requests as req
 
 
-def scrape_all_book_listings(base_url: str, scrape_delay: int=2) -> None:
+def scrape_book_listing_page(body: str) -> list[str]:
+    """Returns a list of book links from a listing page."""
+
+    pass
+
+
+def scrape_all_book_listings(base_url: str, headers: dict, scrape_delay: int = 2) -> None:
     """Loops through all book listing pages until it runs out pages."""
 
     page = 1
@@ -17,7 +25,7 @@ def scrape_all_book_listings(base_url: str, scrape_delay: int=2) -> None:
 
         url = f"{base_url}/books/?page_number={page}"
 
-        res = req.get(url, timeout=5)
+        res = req.get(url, timeout=5, headers=headers)
 
         if "No books found..." in res.text:
             books_on_page = False
@@ -27,7 +35,10 @@ def scrape_all_book_listings(base_url: str, scrape_delay: int=2) -> None:
 
 if __name__ == "__main__":
 
-    URL = "https://publishing.tor.com"
-    SCRAPE_DELAY = 2
+    load_dotenv()
 
-    scrape_all_book_listings(URL, scrape_delay=SCRAPE_DELAY)
+    HEADERS = {
+        "User-Agent": environ["USER_AGENT"]
+    }
+
+    # scrape_all_book_listings(URL, scrape_delay=SCRAPE_DELAY)
